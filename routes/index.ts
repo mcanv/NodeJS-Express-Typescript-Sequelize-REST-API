0f6dd4d16ddf6f1620aa7e7c08f63565b7f80d87
mcanv <mehmetcanvardar49@gmail.com>
2021-11-10 20:18:43 +0300

    İçerikler ve kategoriler routeları ana route bölümüne eklendi

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import HomeController from "../controllers/HomeController";
+import PostRoutes from './posts';
+import CategoryRoutes from './categories';
 
 const router = express.Router();
 
-router.get('/', HomeController.getPosts);
-router.get('/categories', HomeController.getCategories);
+router.use('/api/posts', PostRoutes);
+router.use('/api/categories', CategoryRoutes);
 
 export default router;
