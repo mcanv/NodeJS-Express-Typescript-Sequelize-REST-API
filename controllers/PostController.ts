ac91102666577f912fafa82cd855143886aa7512
mcanv <mehmetcanvardar49@gmail.com>
2021-11-10 20:19:51 +0300

    HomeController PostController olarak güncellendi ve tekil içerik bulma fonksiyonu eklendi.

--- controllers/PostController.ts
+++ controllers/PostController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import Post from "../models/Post";
 import Category from "../models/Category";
 
-export default class HomeController {
+export default class PostController {
     static async getPosts(req: Request, res: Response) : Promise<void> {
         try {
             const posts : Post[] = await Post.findAll({
@@ -16,15 +16,23 @@ export default class HomeController {
         } 
     }
 
-    static async getCategories(req: Request, res: Response) : Promise<void> {
+    static async getPost(req: Request, res: Response) : Promise<void> {
         try {
-            const categories: Category[] = await Category.findAll({
-                include: [Post],
+            const post: Post | any = await Post.findOne({
+                include: [Category],
                 nest: true,
-                raw: false
-            });
-            res.status(200).json(categories);
-        } catch (e) {
+                raw: false,
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            if(post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ msg: 'Post not found!' });
+            }
+        } catch(e) {
             res.status(400).json(e);
         }
     }
