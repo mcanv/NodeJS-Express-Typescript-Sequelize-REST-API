5d811f930f73777c88744edfd11365240ddd051e
mcanv <mehmetcanvardar49@gmail.com>
2021-11-10 20:19:08 +0300

    Arayüzdeki tipler güncellendi

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,9 +1,11 @@
 export interface CategoryAttributes {
+    id: number,
     title: string,
     description: string
 }
 
 export interface PostAttributes {
+    id: number,
     title: string,
     description: string
 }
